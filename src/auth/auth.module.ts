589cb7b5f46534330d91f5a679061daad2accec5
ramin <zoodfekr.r@gmail.com>
2025-12-19 13:12:32 +0330

    auth config

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.jwtConstants_secret || "",
+      signOptions: { expiresIn: '24H' },
+    }),
+  ],
+
+  controllers: [AuthController],
+  providers: [AuthService],
+
+})
+export class AuthModule { }
